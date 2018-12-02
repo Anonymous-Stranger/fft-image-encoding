44d25100b1d49c1dc823555896e656b8b5625a5b
Anonymous-Stranger <akashgaonkar@gmail.com>
2018-12-02 14:56:01 -0700

    Simple dft implementation.

---
+++ dft.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def dft(pts, k_max=None):
+    k_max = k_max or len(pts)
+    return [dftk(pts, k) for k in range(k_max)]
+
+
+def dftk(pts, k):
+    coeff = -2*np.pi*k/len(pts)
+    return sum((x * np.exp(complex(0, coeff*n)) for n, x in enumerate(pts)))
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
+from dft import dft
 from cooley_tukey import cooley_tukey
 
 if __name__== "__main__":
-	print(cooley_tukey(np.array([1,2-1j,-1j,-1+2j])))
+	data = np.array([1,2-1j,-1j,-1+2j])
+	print(cooley_tukey(data))
+	print(map(round, dft(data))

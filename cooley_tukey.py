e3b7fe92aa87a14d43be681fb0c579e431cdd2c9
Anonymous-Stranger <akashgaonkar@gmail.com>
2018-12-10 10:28:12 -0700

    Fixed linting errors (for screenshots)

--- cooley_tukey.py
+++ cooley_tukey.py
@@ -1,13 +1,15 @@
 import numpy as np
 
+
 def cooley_tukey(data):
     data = data.astype(np.complex64)
-    if(data.size==1): return data
+    if(data.size == 1):
+        return data
     even = cooley_tukey(data[::2])
     odd = cooley_tukey(data[1::2])
     multiplier = np.exp(np.pi*-2j/data.size)
     k = 1
-    for i in range(0,odd.size):
+    for i in range(0, odd.size):
         odd[i] *= k
         k *= multiplier
-    return np.concatenate((even+odd,even-odd))
+    return np.concatenate((even+odd, even-odd))
